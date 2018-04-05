817447268cadcb131d2f6d8ee89cb43b0c92c896
Maris Usis <marisusisme@gmail.com>
2018-04-05 14:21:44 +0000

    added commader for control & other fixes adding vorpal need to fix port stuff

---
+++ vizul.js
@@ -0,0 +1,32 @@
+//Get a commander instance
+var program = require('commander');
+
+//Get an instance of winston
+global.winston = require('winston');
+
+const version = '0.0.1';
+
+//Set the version
+program
+  .version(version)
+  .option('-v, --verbose', 'Verbose logging');
+
+//Start command
+program
+  .command('start')
+  .option('-p, --port <port>', 'Set the port for the http server')
+  .action(function(cmd) {
+  //Load the logger
+  if (program.verbose) {
+    global.winston.level = 'debug';
+    global.winston.debug('Using verbose logging');
+  } else global.winston.level = 'info';
+  
+  winston.info('Booting up server...');
+  //Boot up the server!
+  require('./server/index.js',8080,8081)();
+});
+
+
+//Parse input
+program.parse(process.argv);
